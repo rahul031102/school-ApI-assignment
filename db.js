5a4670d3f161505df74d29c9f4b0d52b5855d7b0
rahul031102 <rahuld031102@gmail.com>
2026-03-31 19:37:34 +0530

    use  mysql url

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
+// // const mysql = require("mysql2");
+
+// // const db = mysql.createConnection({
+// //   host: "mysql.railway.internal",
+// //   user: "root",
+// //   password: "kRSoNqGOvpGTNXpnKkeRLlhMQOLXGbNH",
+// //   database: "railway",
+// //   port: "3306"
+// // });
+
+// // db.connect((err) => {
+// //   if (err) {
+// //     console.log("DB Error:", err);
+// //   } else {
+// //     console.log("MySQL Connected");
+// //   }
+// // });
+
+// // module.exports = db;
+
 // const mysql = require("mysql2");
 
 // const db = mysql.createConnection({
-//   host: "mysql.railway.internal",
-//   user: "root",
-//   password: "kRSoNqGOvpGTNXpnKkeRLlhMQOLXGbNH",
-//   database: "railway",
-//   port: "3306"
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   port: process.env.MYSQLPORT
 // });
 
 // db.connect((err) => {
@@ -20,13 +40,7 @@
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
