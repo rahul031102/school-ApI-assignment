857e74422630f457ab26b794a1c5a0c3b3f8a987
rahul031102 <rahuld031102@gmail.com>
2026-03-31 19:04:11 +0530

    deployment changes

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+//   host: "mysql.railway.internal",
+//   user: "root",
+//   password: "kRSoNqGOvpGTNXpnKkeRLlhMQOLXGbNH",
+//   database: "railway",
+//   port: "3306"
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log("DB Error:", err);
+//   } else {
+//     console.log("MySQL Connected");
+//   }
+// });
+
+// module.exports = db;
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "mysql.railway.internal",
-  user: "root",
-  password: "kRSoNqGOvpGTNXpnKkeRLlhMQOLXGbNH",
-  database: "railway",
-  port: "3306"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
