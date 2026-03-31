9e51e649d43153afa855d87912807455879fb433
rahul031102 <rahuld031102@gmail.com>
2026-03-31 18:36:48 +0530

    updated package.json

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "mysql.railway.internal",
   user: "root",
-  password: "1234",
-  database: "schoolDB"
+  password: "kRSoNqGOvpGTNXpnKkeRLlhMQOLXGbNH",
+  database: "railway",
+  port: "3306"
 });
 
 db.connect((err) => {
