2b052ec5c094eda41180255db2cc5a1aebd787dc
rahul031102 <rahuld031102@gmail.com>
2026-04-01 20:37:35 +0530

    variables

--- db.js
+++ db.js
@@ -40,7 +40,15 @@
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.MYSQL_URL);
+//const db = mysql.createConnection(process.env.MYSQL_URL);
+
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
 
 db.connect((err) => {
   if (err) {
